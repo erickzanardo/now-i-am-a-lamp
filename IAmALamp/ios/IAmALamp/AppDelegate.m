a81ef2e7170d708a786a1a4c357aa9a27b489240
Erick <erickzanardoo@gmail.com>
2020-04-26 17:44:38 -0300

    Adding SplashScreen (#8)

--- IAmALamp/ios/IAmALamp/AppDelegate.m
+++ IAmALamp/ios/IAmALamp/AppDelegate.m
@@ -3,6 +3,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "RNSplashScreen.h"  // here
 
 #if DEBUG
 #import <FlipperKit/FlipperClient.h>
@@ -43,6 +44,8 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [RNSplashScreen show];  // here
+
   return YES;
 }
 
