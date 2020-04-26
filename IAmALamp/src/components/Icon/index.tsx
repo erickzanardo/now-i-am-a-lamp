ce1841c7efd18c53b3da5327cbafcd7d22ff1c65
Tyemy Kuga <tyemykuga@gmail.com>
2020-04-25 21:03:09 -0300

    Heart icon button (#5)
    Co-authored-by: Tyemy Kuga <tyemy.kuga@dextra-sw.com>

--- IAmALamp/src/components/Icon/index.tsx
+++ IAmALamp/src/components/Icon/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import {Image} from 'react-native';
+import { Image } from 'react-native';
 
 import mainIcon from '../../../assets/images/mail-icon.png';
+import heartIcon from '../../../assets/images/white-heart.png';
 
-export const MailIcon = () => <Image source={mainIcon} />
+export const MailIcon = () => <Image source={mainIcon} />;
+
+export const HeartIcon = () => <Image source={heartIcon} />;
--- IAmALamp/src/components/PrimaryButton/index.tsx
+++ IAmALamp/src/components/PrimaryButton/index.tsx
@@ -6,11 +6,14 @@ import styles from './styles';
 interface Props {
   onPress: () => void;
   label: string;
+  icon?: React.Component;
 }
 
-export default ({ onPress, label }: Props) => (
+export default ({ onPress, label, icon }: Props) => (
   <TouchableOpacity onPress={onPress}>
     <View style={styles.button}>
+      {icon && <View style={styles.iconContainer}>{icon}</View>}
+
       <Text style={styles.label}>{label?.toUpperCase()}</Text>
     </View>
   </TouchableOpacity>
