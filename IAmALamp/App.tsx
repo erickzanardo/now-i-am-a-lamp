16a56d2c3bff845acb4d0f891686531c848b0e72
Julia Melo <julia.melo@dextra-sw.com>
2020-04-27 13:07:17 -0300

    firebase configs test

--- IAmALamp/App.tsx
+++ IAmALamp/App.tsx
@@ -8,17 +8,30 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { store } from './src/store';
 import 'react-native-gesture-handler';
 
 import Navigator from './src/screens/navigation';
+import { db } from './firebase';
 
 declare const global: { HermesInternal: null | {} };
 
 const App = () => {
+  useEffect(() => {
+    // test firebase - delete after completion
+    console.log("firebase config test")
+    db.collection("user").add({
+      first: "Ada",
+      last: "Lovelace",
+      born: 1815
+    }).then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+    })
+  })
+
   return (
     <View style={{ flex: 1 }}>
       <Provider store={store}>
