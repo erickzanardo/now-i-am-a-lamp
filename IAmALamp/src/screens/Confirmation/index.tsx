d35763710b9dac6e23a6daa29f8ac94aabff2f47
Tyemy Kuga <tyemy.kuga@dextra-sw.com>
2020-04-27 22:58:10 -0300

    Load and change image

--- IAmALamp/src/screens/Confirmation/index.tsx
+++ IAmALamp/src/screens/Confirmation/index.tsx
@@ -1,57 +1,26 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { Route } from '@react-navigation/native';
 
-import BackgroundView from '../../components/BackgroundView';
-import PrimaryButton from '../../components/PrimaryButton';
-import SecondaryButton from '../../components/SecondaryButton';
-import Link from '../../components/Link';
-import { RefreshImageIcon } from '../../components/Icon';
-import { HeartIcon } from '../../components/Icon';
+import Confirmation from './Confirmation';
+import { search } from '../../actions/searchImage';
 
-import styles from './styles';
+interface Props {
+  route: Route<any>;
+}
 
-const Register = () => {
+const ConfirmationContainer = ({ route }: Props) => {
+  const dispatch = useDispatch();
+
+  //@ts-ignore
+  const { objectText } = route.params;
+
   return (
-    <BackgroundView customStyle={styles.bodyWrapper}>
-      <View style={styles.body}>
-        <View style={styles.objectImage}>
-          <View style={styles.refreshImage}>
-            <Link onPress={() => {}}>
-              <RefreshImageIcon />
-            </Link>
-          </View>
-        </View>
-        <View style={styles.titleContainer}>
-          <Text style={styles.titleText}>
-            Yeah! You’re a<Text style={styles.titleObjectText}> Shoe </Text>now
-          </Text>
-        </View>
-        <View style={styles.subTitleContainer}>
-          <Text style={styles.subTitleTextNumber}>32</Text>
-          <Text style={styles.subTitleText}>
-            people are living as
-            <Text style={styles.subTitleObjectText}> Shoes </Text>right now.
-          </Text>
-        </View>
-        <View style={styles.lifeText}>
-          <Text style={styles.subTitleText}>
-            Are you ok to live
-            <Text style={styles.subTitleObjectText}> 7 days </Text>as a
-            <Text style={styles.subTitleObjectText}> Shoe</Text>?
-          </Text>
-        </View>
-      </View>
-      <View style={styles.buttonsContainer}>
-        <PrimaryButton
-          width={165}
-          onPress={() => {}}
-          label="Yes!!!"
-          icon={<HeartIcon />}
-        />
-        <SecondaryButton width={165} onPress={() => {}} label="Cancel" />
-      </View>
-    </BackgroundView>
+    <Confirmation
+      fetchImages={(text: string, page: number) => search(text, page)}
+      objectText={objectText}
+    />
   );
 };
 
-export default Register;
+export default ConfirmationContainer;
