d8afaa163f6f210ffa9f70de36d774607f2f02b2
Marcio Souza <marciomultimedia@gmail.com>
2020-04-27 21:24:36 -0300

    fix: duplicated text import

--- IAmALamp/storybook/stories/index.js
+++ IAmALamp/storybook/stories/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Text } from 'react-native';
-
-import { Text } from 'react-native';
 import { storiesOf } from '@storybook/react-native';
 
 import PrimaryButton from '../../src/components/PrimaryButton';
